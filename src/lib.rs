58d5f224d86a3b99ded1ca0f77ba98b8d96cada4
Matilde Morrone <contact@morrone.dev>
2022-08-12 20:00:10 +0200

    Allow unsafe_impl on non unix platforms

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(unchecked_math))]
 
-#[cfg(unix)]
+#[cfg(any(unix, feature = "unsafe_impl"))]
 mod unix;
 #[cfg(windows)]
 mod win32;
 
-#[cfg(unix)]
+#[cfg(any(unix, feature = "unsafe_impl"))]
 pub use unix::*;
 #[cfg(windows)]
 pub use win32::*;
